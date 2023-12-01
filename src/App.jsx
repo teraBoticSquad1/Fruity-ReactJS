9efbe5fc6096253c8f1a402bd49dab58ec375d3f
“NhShanto01” <mdnurhossain940@gmail.com>
2023-12-01 14:16:22 +0600

    started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <p>This is Fruity Project</p>
+    </div>
+  );
+}
+
+export default App;
